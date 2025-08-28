46d278a0f4dbcf438b0c446030a961e6495c6298
Edwin Orellana <educsis@gmail.com>
2025-08-28 12:23:03 -0600

    Initial commit: Setup Nuxt.js travel-log project

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <h1>Calzado Jordan EsLint</h1>
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-secondary">
+    Secondary
+  </button>
+  <br>
+  <hr>
+  <Icon
+    name="uil:github"
+    style="color: black"
+    size="34"
+  />
+  <hr>
+  <Icon
+    name="svg-spinners:clock"
+    style="color: black"
+    size="34"
+  />
+  <br>
+  <button class="btn btn-primary">
+    <Icon
+      name="svg-spinners:clock"
+      style="color: white"
+      size="34"
+    /> Hello world!
+  </button>
 </template>
